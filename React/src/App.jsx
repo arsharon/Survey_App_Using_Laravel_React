e278aacb878c618cb55b58310992bf2a435e431c
Adnan Rahman <arahman4946@gmail.com>
2024-05-22 23:42:27 +0600

    Vite, Hero Icon, Tailwind Css installed

---
+++ React/src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+import {TruckIcon} from '@heroicons/react/24/solid'
+
+export default function App() {
+  return (
+    <h1 className="bg-purple-500 underline">
+      Hello world!
+      <TruckIcon className='w-4'/>    
+    </h1>
+  )
+}
